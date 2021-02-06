f4e4e94c2616a2c1ec147b7ef7dcc1b45c3ca831
Joao Vitor Carli <jvitor.carli@tutamail.com>
2021-02-06 15:14:12 -0300

    [FEATURE] Update assets/js/index.js and templates/hello-world.html to use React buttons.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello webpack';
-  return element;
-}
+class Buttons extends React.Component {
+    render() {
+        return(
+            <>
+                <button
+                    className="transition px-6 py-4 shadow-xl rounded-xl duration-500 ease-in-out bg-purple-600 hover:bg-yellow-600 transform hover:-translate-y-1 hover:scale-110 ...">
+                    <a href="https://docs.djangoproject.com/" className="text-white font-medium text-3xl">Django Docs</a>
+                </button>
 
-document.body.appendChild(component());
+                <button
+                    className="transition px-6 py-4 shadow-xl rounded-xl duration-500 ease-in-out bg-purple-600 hover:bg-yellow-600 transform hover:-translate-y-1 hover:scale-110 ...">
+                    <a href="https://tailwindcss.com/" className="text-white font-medium text-3xl">TailwindCSS Docs</a>
+                </button>
+
+                <button
+                    className="transition px-6 py-4 shadow-xl rounded-xl duration-500 ease-in-out bg-purple-600 hover:bg-yellow-600 transform hover:-translate-y-1 hover:scale-110 ...">
+                    <a href="https://www.reactjs.com/" className="text-white font-medium text-3xl">React Docs</a>
+                </button>
+            </>
+        )
+    }
+}
 
 // Our first react usage
 ReactDOM.render(
-  <h1>Hello, react!</h1>,
-  document.getElementById('root')
-);
+    <Buttons/>,
+    document.getElementById('react_buttons')
+)
