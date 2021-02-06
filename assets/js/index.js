b353b14bef1a79966e7c12d9fb980856e7432e43
Joao Vitor Carli <jvitor.carli@tutamail.com>
2021-02-06 16:43:28 -0300

    [FEATURE] Add responsive css to templates/hello_world.html.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-class Buttons extends React.Component {
+class DocsButtons extends React.Component {
     render() {
         return(
             <>
                 <button
-                    className="transition px-6 py-4 shadow-xl rounded-xl duration-500 ease-in-out bg-purple-600 hover:bg-yellow-600 transform hover:-translate-y-1 hover:scale-110 ...">
-                    <a href="https://docs.djangoproject.com/" className="text-white font-medium text-3xl">Django Docs</a>
+                    className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
+                        <a href="https://docs.djangoproject.com/" className="text-white font-medium text-xl lg:text-2xl xl:text-3xl">
+                            Django Docs
+                        </a>
                 </button>
 
                 <button
-                    className="transition px-6 py-4 shadow-xl rounded-xl duration-500 ease-in-out bg-purple-600 hover:bg-yellow-600 transform hover:-translate-y-1 hover:scale-110 ...">
-                    <a href="https://tailwindcss.com/" className="text-white font-medium text-3xl">TailwindCSS Docs</a>
+                    className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
+                        <a href="https://tailwindcss.com/" className="text-white font-medium text-xl lg:text-2xl xl:text-3xl">
+                            TailwindCSS Docs
+                        </a>
                 </button>
 
                 <button
-                    className="transition px-6 py-4 shadow-xl rounded-xl duration-500 ease-in-out bg-purple-600 hover:bg-yellow-600 transform hover:-translate-y-1 hover:scale-110 ...">
-                    <a href="https://www.reactjs.com/" className="text-white font-medium text-3xl">React Docs</a>
+                    className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
+                        <a href="https://www.reactjs.org/" className="text-white font-medium lg:text-2xl xl:text-3xl">
+                            React Docs
+                        </a>
                 </button>
             </>
         )
@@ -26,6 +32,6 @@ class Buttons extends React.Component {
 
 // Our first react usage
 ReactDOM.render(
-    <Buttons/>,
+    <DocsButtons/>,
     document.getElementById('react_buttons')
 )
