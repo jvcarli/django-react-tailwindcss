024e9ce19baf90348a036f3affff21334103f06a
Joao Vitor Carli <jvitor.carli@tutamail.com>
2021-02-06 16:56:10 -0300

    [BUGFIX] Fix buttons css on small breakpoint.

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,21 +7,22 @@ class DocsButtons extends React.Component {
             <>
                 <button
                     className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
-                        <a href="https://docs.djangoproject.com/" className="text-white font-medium text-xl lg:text-2xl xl:text-3xl">
-                            Django Docs
-                        </a>
+                    <a href="https://docs.djangoproject.com/" className="text-white font-medium text-xl lg:text-2xl xl:text-3xl">
+                        Django Docs
+                    </a>
                 </button>
 
                 <button
                     className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
-                        <a href="https://tailwindcss.com/" className="text-white font-medium text-xl lg:text-2xl xl:text-3xl">
-                            TailwindCSS Docs
-                        </a>
+                    <a href="https://tailwindcss.com/" className="text-white font-medium text-xl lg:text-2xl xl:text-3xl">
+                        TailwindCSS Docs
+                    </a>
                 </button>
+
 
                 <button
                     className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
-                        <a href="https://www.reactjs.org/" className="text-white font-medium lg:text-2xl xl:text-3xl">
+                        <a href="https://www.reactjs.org/" className="text-white font-medium text-xl lg:text-2xl xl:text-3xl">
                             React Docs
                         </a>
                 </button>
