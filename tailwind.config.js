473b4f1b90d56d588e8519ebd26e67089d5d5391
Joao Vitor Carli <jvitor.carli@tutamail.com>
2021-02-06 12:54:18 -0300

    [INTERNAL] Add TailwindCSS dependencies and config files.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: {
+    enabled: false,  //true for production build
+    content: [
+      './templates/*.html',  // root templates dir
+      './templates/**/*.html',  // root templates subdir
+      './**/templates/*.html',  // pkgs templates dir
+      './**/templates/**/*.html',  // pkgs templates subdir, mainly react templates
+      './assets/ts/*.{ts,tsx}',  // typescript files, mainly react templates
+      './assets/ts/**/*.{ts,tsx}',
+      './assets/js/*.js',  // javascript files, mainly react templates
+      './assets/js/**/*.js',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
