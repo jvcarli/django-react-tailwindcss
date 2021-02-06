6b8b1bff5217b329117317bcff8ef1a00a6b5d39
Joao Vitor Carli <jvitor.carli@tutamail.com>
2021-02-06 14:26:39 -0300

    [FEATURE] Update bootstrap.sh script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -c 'from django.core.management.utils import get_random_secret_key; print("SECRET_KEY = \"" + get_random_secret_key() + "\"")' >> ./django_react_tailwindcss/settings.py
+pipenv run python -c 'from django.core.management.utils import get_random_secret_key; print("SECRET_KEY = \"" + get_random_secret_key() + "\"")' >> ./django_react_tailwindcss/settings.py
