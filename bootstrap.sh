7d074437043ccd23d729f90fd81873d2baa3e776
João Vítor Carli <jvitor.carli@tutamail.com>
2021-04-19 09:15:13 -0300

    [INTERNAL] Update django_react_tailwindcss/setting.py, bootstrap.sh and uptade security section in README.md accordingly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pipenv run python -c 'from django.core.management.utils import get_random_secret_key; print("SECRET_KEY=" + get_random_secret_key())' >> .env
+pipenv run python -c 'from django.core.management.utils import get_random_secret_key; print("DEV_SECRET_KEY=" + get_random_secret_key())' >> .env
--- django_react_tailwindcss/settings.py
+++ django_react_tailwindcss/settings.py
@@ -133,4 +133,4 @@ if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
 # SECRET KEY
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['DEV_SECRET_KEY']
