608b4105b826822d04f5c2fd7629b6b00f026b18
João Vítor Carli <jvitor.carli@tutamail.com>
2021-04-19 01:06:19 -0300

    [FEATURE] Make react code in assets/js/index.js more idiomatic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class DocsButtons extends React.Component {
-  render() {
-    return (
-      <>
-        <button className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
-          <a
-            href="https://docs.djangoproject.com/"
-            className="text-white font-medium text-xl lg:text-2xl xl:text-3xl"
-          >
-            Django Docs
-          </a>
-        </button>
+const docsLinkList = [
+  { name: "Django Docs", href: "https://docs.djangoproject.com/" },
+  { name: "TailwindCSS Docs", href: "https://tailwindcss.com/" },
+  { name: "React Docs", href: "https://www.reactjs.org/" },
+];
 
-        <button className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
-          <a
-            href="https://tailwindcss.com/"
-            className="text-white font-medium text-xl lg:text-2xl xl:text-3xl"
-          >
-            TailwindCSS Docs
-          </a>
-        </button>
+const Button = ({ value, href }) => (
+  <button className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
+    <a
+      className="text-white font-medium text-xl lg:text-2xl xl:text-3xl"
+      href={href}
+    >
+      {value}
+    </a>
+  </button>
+);
 
-        <button className="transition px-6 py-4 shadow-xl rounded-xl xl:duration-500 xl:ease-in-out bg-purple-600 xl:hover:bg-yellow-600 transform xl:hover:-translate-y-1 xl:hover:scale-110">
-          <a
-            href="https://www.reactjs.org/"
-            className="text-white font-medium text-xl lg:text-2xl xl:text-3xl"
-          >
-            React Docs
-          </a>
-        </button>
-      </>
-    );
-  }
-}
+const DocsButtons = () =>
+  docsLinkList.map((link) => <Button value={link.name} href={link.href} />);
 
 // Our first react usage
 ReactDOM.render(<DocsButtons />, document.getElementById("react_buttons"));
